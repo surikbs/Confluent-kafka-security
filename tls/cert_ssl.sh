f969cf31d7b72e14ed8422f5c61f83634cfb1a3f
surikbs <surikbs@gmail.com>
2020-06-24 14:24:47 +0530

    initial commit

---
+++ tls/cert_ssl.sh
@@ -0,0 +1,22 @@
+# Creating TLS CA, Certificates and keystore / truststore
+rm -rf certs 
+mkdir -p certs
+
+# Generate CA certificates
+openssl req -new -nodes -x509 -days 3650 -newkey rsa:2048 -keyout certs/ca.key -out certs/ca.crt -config ca.cnf 
+cat certs/ca.crt certs/ca.key > certs/ca.pem
+
+# Generate kafka broker certificates
+openssl req -new -newkey rsa:2048 -keyout certs/broker-server.key -out certs/broker-server.csr -config broker-server.cnf -nodes
+openssl x509 -req -days 3650 -in certs/broker-server.csr -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/broker-server.crt -extfile broker-server.cnf -extensions v3_req
+openssl pkcs12 -export -in certs/broker-server.crt -inkey certs/broker-server.key -chain -CAfile certs/ca.pem -name "kafka.confluent.local" -out certs/broker-server.p12 -password pass:test1234
+
+# Import server certificate to keystore and CA to truststore
+keytool -importkeystore -deststorepass test1234 -destkeystore certs/broker-server.keystore.jks \
+    -srckeystore certs/broker-server.p12 \
+    -deststoretype PKCS12  \
+    -srcstoretype PKCS12 \
+    -noprompt \
+    -srcstorepass test1234
+
+keytool -keystore certs/truststore.jks -alias CARoot -import -file certs/ca.crt -storepass test1234  -noprompt -storetype PKCS12 
