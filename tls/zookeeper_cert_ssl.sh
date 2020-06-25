2dabc45e0c416eb740489d86d081ec7929fcf4ff
surikbs <surikbs@gmail.com>
2020-06-25 12:15:03 +0530

    Update zookeeper_cert_ssl.sh

--- tls/zookeeper_cert_ssl.sh
+++ tls/zookeeper_cert_ssl.sh
@@ -1,7 +1,7 @@
 
 # Generate  Zookeeper certificates
 
-openssl req -new -newkey rsa:2048 -keyout certs/zookeeper.key -out certs/zookeeper.csr -config certs/zookeeper.cnf -nodes
+openssl req -new -newkey rsa:2048 -keyout certs/zookeeper.key -out certs/zookeeper.csr -config zookeeper.cnf -nodes
 openssl x509 -req -days 3650 -in certs/zookeeper.csr -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/zookeeper.crt -extfile zookeeper.cnf -extensions v3_req
 openssl pkcs12 -export -in certs/zookeeper.crt -inkey certs/zookeeper.key -chain -CAfile certs/ca.pem -name "Zookeeper.Confluent" -out certs/zookeeper.p12 -password pass:test1234
 
